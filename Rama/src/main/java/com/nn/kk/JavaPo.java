ff2b27b2c4abaac055643f2985a8df0f7abfb8f4
koti <kkr.kothapally@gmail.com>
2016-10-05 12:34:41 +0530

    commit 2

--- Rama/src/main/java/com/nn/kk/JavaPo.java
+++ Rama/src/main/java/com/nn/kk/JavaPo.java
@@ -5,6 +5,7 @@ public class JavaPo {
 	public static void main(String args[])
 	{
 		System.out.println("hello Mr.SaiBabau garu how are you...");
+		System.out.println("heloo MR.koti how are you.....");
 	}
 
 }
